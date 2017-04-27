6c2c2c50372a2fad5a27011e68699464c1902c2d
MAURICIO SUAZA <mauriciosuazauribe@gmail.com>
2017-04-27 15:30:48 -0500

    forced sssl

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,11 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+config.force_ssl = true
+
 module SpreeDemo
   class Application < Rails::Application
-    
+
     config.to_prepare do
       # Load application's model / class decorators
       Dir.glob(File.join(File.dirname(__FILE__), "../app/**/*_decorator*.rb")) do |c|
