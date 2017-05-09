3d7a325921d3b6f9d4e56c0c31d9c98594f24c6c
kevanz10 <krojas1171@gmail.com>
2017-05-09 16:33:22 -0500

    fixed routes conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,11 @@ Rails.application.routes.draw do
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
-  mount Spree::Core::Engine, at: '/'
+  mount Spree::Core::Engine, at: "/"
 
   get '/tracker', to: 'shippings#tracker', as: 'track_shipment'
-
+  get 'home_page' => 'spree/home#home_page'
+  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
