2ea468b71fc21bc215e46eedc646c4c73ef1c7cf
kevanz10 <krojas1171@gmail.com>
2017-05-17 16:49:03 -0500

    added new styles in home page, mobile version home and about page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,7 +59,9 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener
+
+  # config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.default :charset => "utf-8"
 
