d4958c2a510eced44aba2602c09a79143af01ed1
kevanz10 <krojas1171@gmail.com>
2017-06-24 09:33:42 -0500

    added more views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,50 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+jQuery(document).ready(function($) {
+  $(".photo1").mouseover(function(e){    
+    $(".block-text").css("visibility", "visible");
+  });
+  $(".photo1").mouseout(function(e){
+    $(".block-text").css("visibility", "hidden"); 
+  });
+
+  $(".photo2").mouseover(function(e){    
+    $(".block-text2").css("visibility", "visible");
+  });
+  $(".photo2").mouseout(function(e){
+    $(".block-text2").css("visibility", "hidden"); 
+  });
+
+  $(".photo3").mouseover(function(e){    
+    $(".block-text3").css("visibility", "visible");
+  });
+  $(".photo3").mouseout(function(e){
+    $(".block-text3").css("visibility", "hidden"); 
+  });
+
+  $(".sedes-bg .color1").mouseover(function(e){
+    $(".one").css("visibility", "visible"); 
+  });
+
+  $(".sedes-bg .color1").mouseout(function(e){
+    $(".one").css("visibility", "hidden"); 
+  });
+
+  $(".sedes-bg .color2").mouseover(function(e){
+    $(".second").css("visibility", "visible"); 
+  });
+
+  $(".sedes-bg .color2").mouseout(function(e){
+    $(".second").css("visibility", "hidden"); 
+  });
+
+  $(".sedes-bg .color3").mouseover(function(e){
+    $(".third").css("visibility", "visible"); 
+  });
+
+  $(".sedes-bg .color3").mouseout(function(e){
+    $(".third").css("visibility", "hidden"); 
+  });
+});
